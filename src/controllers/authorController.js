6839c58f9e3bd55de2cc5c6ee54dcdf57ee7abcb
GokoshiJr <julioagonzalez18@gmail.com>
2020-11-17 22:53:03 -0400

    Add partials to view html

--- src/controllers/authorController.js
+++ src/controllers/authorController.js
@@ -1,9 +1,16 @@
 const controller = {};
 
 controller.list = (req, res) => {
-  
-  req.getConnection((err, conexi) => {
-    res.send('Hola mundo soy Gokoshi Jr');
+  req.getConnection((err, conexi) => { 
+    conexi.query('SELECT * FROM authors;', (err, rows) => { // rows es un array
+      if (err) {
+        res.json(err);
+      } else {
+        res.render('authors', { // renderiza authors.ejs
+          data: rows // le pasamos las filas de mysql a la vista ejs
+        }); 
+      }
+    });
   }); // agrega a req ese metodo getConnection del paquete myconnection
 };
 
