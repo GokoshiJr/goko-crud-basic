0111031b71c4c1119a384009ca921640a4731aa7
GokoshiJr <julioagonzalez18@gmail.com>
2020-11-20 19:41:56 -0400

    Add Prettier

--- src/main.js
+++ src/main.js
@@ -1,48 +1,48 @@
-const express = require('express'); // framework de node
-const path = require('path'); // nativo de node
-const morgan = require('morgan'); // middleware
-const { urlencoded } = require('express'); // para recibir los datos del form html
+const express = require("express"); // framework de node
+const path = require("path"); // nativo de node
+const morgan = require("morgan"); // middleware
+const { urlencoded } = require("express"); // para recibir los datos del form html
 
 // para la conexion con la bd
-const mysql = require('mysql');
-const mysqlConnection = require('express-myconnection'); 
+const mysql = require("mysql");
+const mysqlConnection = require("express-myconnection");
 
 // instanciamos una funcion de express que seria el servidor
 const app = express();
 
 // settings
-app.set('port', process.env.PORT || 3000);
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("port", process.env.PORT || 3000);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 // bd settings
 const database = {
-  host: 'localhost',
-  user: 'root',
-  password: '$R5t6u8.',
+  host: "localhost",
+  user: "root",
+  password: "$R5t6u8.",
   port: 3306,
-  database: 'goko_operation'
-}
+  database: "goko_operation",
+};
 
 // middlewares
-app.use(morgan('dev'));
-app.use(mysqlConnection(mysql, database, 'single'));
-app.use(urlencoded({extended: false})); 
+app.use(morgan("dev"));
+app.use(mysqlConnection(mysql, database, "single"));
+app.use(urlencoded({ extended: false }));
 /* 
   urlencoded, permite entender los datos que vienen desde el formulario de html, 
   false porque no recibimos imagenes o nada encriptado 
 */
 
 // importing routes
-const userRoutes = require('./routes/author');
+const userRoutes = require("./routes/author");
 
 // routes
-app.use('/', userRoutes);
+app.use("/", userRoutes);
 
 // static files
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // starting the server
-app.listen(app.get('port'), () => {
+app.listen(app.get("port"), () => {
   console.log(`Server on port 3000`);
 });
--- src/routes/author.js
+++ src/routes/author.js
@@ -1,15 +1,15 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const authorController = require('../controllers/authorController');
+const authorController = require("../controllers/authorController");
 
-router.get('/', authorController.list);
+router.get("/", authorController.list);
 
-router.post('/add', authorController.save);
+router.post("/add", authorController.save);
 
-router.get('/delete/:id', authorController.delete); // :id parametro de la ruta
+router.get("/delete/:id", authorController.delete); // :id parametro de la ruta
 
-router.get('/update/:id', authorController.edit);
-router.post('/update/:id', authorController.editUpdate);
+router.get("/update/:id", authorController.edit);
+router.post("/update/:id", authorController.editUpdate);
 
 module.exports = router;
